540c93b5a87111909878562ebc7e362527be139d
孔祥飞 <kongxiangfei@okjiaoyu.cn>
2016-07-20 13:38:47 +0800

    dubbo的provider实现

--- my-dubbo-user-main/src/main/java/com/king/service/first/impl/FirstServiceImpl.java
+++ my-dubbo-user-main/src/main/java/com/king/service/first/impl/FirstServiceImpl.java
@@ -1,12 +1,14 @@
-package com.king;
+package com.king.service.first.impl;
 
 import com.king.service.FirstService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by xiangfei on 16/7/18.
  */
+@Service("firstService")
 public class FirstServiceImpl implements FirstService {
     public String getString() {
-        return null;
+        return "Good morning!";
     }
 }
